c44155a35f4138999c8e9a0fc2d09470c719c45b
tomasmali <megatoys92@gmail.ocm>
2020-01-05 21:59:38 +0100

    aggiunti gestione

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {  HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-menu',
@@ -7,9 +8,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuComponent implements OnInit {
 
-  constructor() { }
+  response: any;
+
+
+
+  constructor(public http: HttpClient) { 
+    this.http
+    .get('http://93.49.6.246:3000/menu').subscribe((response)=> {
+      console.log(response)
+      
+      this.response = response
+
+    })
+  }
 
   ngOnInit() {
+
   }
+
+
+
+getTables(){
+  this.http
+    .get('http://93.49.6.246:3000/menu').subscribe((response)=> {
+      console.log(response)
+      this.response = response
+    })
+}
 
 }
--- src/app/tavole/tavole.component.ts
+++ src/app/tavole/tavole.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {  HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-tavole',
@@ -7,9 +8,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TavoleComponent implements OnInit {
 
-  constructor() { }
+  response: any;
+
+  allUsers: []
+
+  constructor(public http: HttpClient) { 
+    this.http
+    .get('http://93.49.6.246:3000/tavola/622406760').subscribe((response)=> {
+      console.log(response)
+      
+      this.response = response
+
+    })
+  }
 
   ngOnInit() {
+
   }
+
+
+
+getTables(){
+  this.http
+    .get('http://93.49.6.246:3000/tavola/622406760').subscribe((response)=> {
+      console.log(response)
+      this.response = response
+    })
+}
 
 }
